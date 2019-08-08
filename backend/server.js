4725a4ddf4127c393fc54cb50939a0d48a346c77
unknown <andrei_bulzan2005@yahoo.com>
2019-08-08 17:18:31 +0300

    asd

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,34 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+var Cloudant = require('@cloudant/cloudant');
+
+const port = process.env.PORT || 4000;
+
+const cloudant = Cloudant({
+    
+    url:'https://05e8d0f1-0abd-463f-be62-6935ef2bea01-bluemix.cloudantnosqldb.appdomain.cloud',
+    plugins:
+    {
+        iamauth:
+        {
+            iamApiKey:'--ajLozzvXDvABWpf7lDozCZCBB7IgqLrlOnNja31ott'
+        }
+    }
+},async(err, client) => {
+        const dbList = await client.db.list();
+        const dbExists = dbList.indexOf('db_test') !== -1;
+        if(!dbExists)
+        {
+            client.db.create('db_test');
+        }
+
+    
+});
+
 app.use(cors());
 app.use(bodyParser.json());
-var Cloudant = require('@cloudant/cloudant');
+
 
 let todos=[];
 
@@ -30,19 +55,23 @@ app.post('/checked/:id', (req,res)=>{
     res.send({success:true});
 }
 )
-app.post('/add', (req,res)=>{
+app.post('/add',async (req,res)=>{
     const todo = req.body.todo;
     todos.push(todo);
     console.log('todos',todos);
+    await cloudant.db.use('db_test').insert(todo);
     res.send({success:true});
+
 }
 )
-app.get('/getTodos', (req,res)=>{
+app.get('/getTodos',async (req,res)=>{
+    //const dbTodos = await cloudant.db.use'db_test'.find;
+
     res.send({todos})
 }
 )
 
-app.listen(4000, (req, res)=>
+app.listen(port, (req, res)=>
 {
     console.log('Server is running')
 })
